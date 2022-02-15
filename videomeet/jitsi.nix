df5aba5fa5563a98f035ea5ca2b2e7afeb16a3a0
inexcode <inex.code@selfprivacy.org>
2022-02-15 15:48:42 +0300

    Revert "Merge pull request 'Fixed Jitsi functionality and Jitsi certificate resolution' (#14) from jitsi-fixes into master"
    This reverts commit dc767677d85133dae069e8c8e8f063d2549fb29e, reversing
    changes made to d4bb3816934356051c3a83909c268ee438d18029.

--- videomeet/jitsi.nix
+++ videomeet/jitsi.nix
@@ -6,7 +6,7 @@ in
   services.jitsi-meet = {
     enable = config.services.userdata.jitsi.enable;
     hostName = "meet.${domain}";
-    nginx.enable = true;
+    nginx.enable = false;
     interfaceConfig = {
       SHOW_JITSI_WATERMARK = false;
       SHOW_WATERMARK_FOR_GUESTS = false;
