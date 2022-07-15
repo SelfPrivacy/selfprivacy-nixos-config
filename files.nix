6e6d21af626f72f58b491a23650508b4d209f695
inexcode <inex.code@selfprivacy.org>
2022-07-15 18:20:10 +0300

    Ensure folders for activation script are created

--- files.nix
+++ files.nix
@@ -11,6 +11,8 @@ in
       (if cfg.bitwarden.enable then "d /var/lib/bitwarden 0777 vaultwarden vaultwarden -" else "")
       (if cfg.bitwarden.enable then "d /var/lib/bitwarden/backup 0777 vaultwarden vaultwarden -" else "")
       (if cfg.pleroma.enable then "d /var/lib/pleroma 0700 pleroma pleroma - -" else "")
+      "d /val/lib/cloudflare 0440 nginx acmerecievers -"
+      "d /root/.config/rclone/ 0400 root root -"
       "d /var/lib/restic 0600 restic - - -"
       (if cfg.pleroma.enable then "f /var/lib/pleroma/secrets.exs 0755 pleroma pleroma - -" else "")
       "f+ /var/domain 0444 selfprivacy-api selfprivacy-api - ${domain}"
