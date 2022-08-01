1418ac7c554a3b0be0e6738e61dd92455f88fddd
Inex Code <inex.code@selfprivacy.org>
2022-08-01 22:32:54 +0300

    Fix volumes

--- volumes.nix
+++ volumes.nix
@@ -3,15 +3,13 @@ let
   cfg = config.services.userdata;
 in
 {
-  fileSystems =
-    builtins.listToAttrs
-      (builtins.map
-        (volume: {
-          name: "${volume.mountPoint}";
-          value: {
-          device = "${volume.device}";
-          fsType = "${volume.fsType}";
-        };
-        })
-  cfg.volumes);
+  fileSystems = { } // builtins.listToAttrs (builtins.map
+    (volume: {
+      name = "${volume.mountPoint}";
+      value = {
+        device = "${volume.device}";
+        fsType = "${volume.fsType}";
+      };
+    })
+    cfg.volumes);
 }
