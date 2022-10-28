399790e2026f35d486d5bb856dca281195f74f47
inexcode <inex.code@selfprivacy.org>
2022-10-28 11:52:47 +0300

    chore(nextcloud): Upgrade Nextcloud to v24

--- nextcloud/nextcloud.nix
+++ nextcloud/nextcloud.nix
@@ -11,7 +11,7 @@ in
   };
   services.nextcloud = {
     enable = cfg.nextcloud.enable;
-    package = pkgs.nextcloud23;
+    package = pkgs.nextcloud24;
     hostName = "cloud.${cfg.domain}";
 
     # Use HTTPS for links
