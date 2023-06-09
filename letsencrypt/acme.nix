dd020c3a7d5597ff150e015a64f592eddd167aa9
Inex Code <inex.code@selfprivacy.org>
2023-06-09 15:57:19 +0300

    fix(acme): Disable DNS propagation check

--- letsencrypt/acme.nix
+++ letsencrypt/acme.nix
@@ -11,6 +11,7 @@ in
     defaults = {
       email = "${cfg.username}@${cfg.domain}";
       server = if cfg.dns.useStagingACME then "https://acme-staging-v02.api.letsencrypt.org/directory" else "https://acme-v02.api.letsencrypt.org/directory";
+      dnsPropagationCheck = false;
     };
     certs = lib.mkForce {
       "${cfg.domain}" = {
