a822f97699165bba81359a176ef9287196efa501
Alexander Tomokhov <alexoundos@xunison.com>
2023-07-17 04:58:26 +0400

    pin dependencies in flake

--- flake.nix
+++ flake.nix
@@ -3,10 +3,9 @@
 
   inputs = {
     #nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/eef86b8a942913a828b9ef13722835f359deef29.tar.gz";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-    # selfprivacy-overlay.url = "https://git.selfprivacy.org/SelfPrivacy/selfprivacy-nix-repo/archive/22-11.tar.gz";
-    selfprivacy-overlay.url = "git+file:///data/selfprivacy/selfprivacy-nix-repo";
-    # userdata-json.url = ./userdata/userdata.json;
+    nixpkgs.url = "github:nixos/nixpkgs";
+    selfprivacy-overlay.url =
+      "git+https://git.selfprivacy.org/SelfPrivacy/selfprivacy-nix-repo.git";
   };
 
   outputs = { self, nixpkgs, selfprivacy-overlay }:
