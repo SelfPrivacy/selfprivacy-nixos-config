6cd002ae1d4b3307092d8c39e5a77daf0fbca9de
Alexander Tomokhov <alexoundos@selfprivacy.org>
2023-12-05 01:56:13 +0400

    remove restic SP module

--- sp-modules/restic/flake.nix
+++
@@ -1,9 +0,0 @@
-{
-  description = "PoC SP module for Restic backup service";
-
-  outputs = { self }: {
-    nixosModules.default = import ./module.nix;
-    configPathsNeeded =
-      builtins.fromJSON (builtins.readFile ./config-paths-needed.json);
-  };
-}
