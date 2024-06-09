26c9fd5f6e6871056144ee9b7d14984f63c5d3a4
nhnn <nhnn@disroot.org>
2024-06-09 21:04:14 +0300

    feat: add prometheus monitoring service

---
+++ sp-modules/monitoring/module.nix
@@ -0,0 +1,31 @@
+{config, lib, ...}: let
+  cfg = config.selfprivacy.modules.monitoring;
+in {
+  options.selfprivacy.modules.monitoring = {
+    enable = lib.mkOption {
+      default = false;
+      type = lib.types.bool;
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    services.prometheus = {
+      enable = true;
+      port = 9001;
+      exporters = {
+        node = {
+          enable = true;
+          enabledCollectors = [ "systemd" ];
+          port = 9002;
+        };
+      };
+      scrapeConfigs = [
+        {
+          job_name = "node-exporter";
+          static_configs = [{
+            targets = [ "127.0.0.1:9002" ];
+          }];
+        }
+      ];
+    };
+  };
+}
