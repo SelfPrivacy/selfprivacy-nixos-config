f02b4d46c846c105f0382ba1308239ab8200c22c
Inex Code <inex.code@selfprivacy.org>
2024-10-24 22:21:15 +0300

    nextcloud 27 downgrade

--- sp-modules/nextcloud/flake.nix
+++ sp-modules/nextcloud/flake.nix
@@ -1,19 +1,10 @@
 {
   description = "PoC SP module for nextcloud";
 
-  inputs.nixpkgs-old.url = "nixpkgs/nixos-23.11";
-
-  outputs = { self, nixpkgs-old }:
-    let
-      oldPkgs = import nixpkgs-old {
-        system = "x86_64-linux"; # Same architecture as above
-      };
-    in
-    {
+  outputs = { self }: {
       nixosModules.default = _:
         {
           imports = [ ./module.nix ./cleanup-module.nix ];
-          services.nextcloud.package = oldPkgs.nextcloud26;
         };
       configPathsNeeded =
         builtins.fromJSON (builtins.readFile ./config-paths-needed.json);
--- sp-modules/nextcloud/module.nix
+++ sp-modules/nextcloud/module.nix
@@ -66,7 +66,7 @@
       };
       services.nextcloud = {
         enable = true;
-        #package = oldPkgs.nextcloud26;
+        package = pkgs.nextcloud27;
         inherit hostName;
 
         # Use HTTPS for links
