f19888accdc2b4ca8092500cff6ed33889d6c31c
Inex Code <inex.code@selfprivacy.org>
2025-01-29 14:47:06 +0300

    allow plain login

--- sp-modules/simple-nixos-mailserver/auth-dovecot.nix
+++ sp-modules/simple-nixos-mailserver/auth-dovecot.nix
@@ -85,7 +85,7 @@ in
   };
 
   services.dovecot2.extraConfig = ''
-    auth_mechanisms = xoauth2 oauthbearer
+    auth_mechanisms = xoauth2 oauthbearer plain login
 
     passdb {
       driver = oauth2
@@ -95,6 +95,7 @@ in
 
     passdb {
       driver = checkpassword
+      mech = plain login
       args = ${dovecot-auth-script}/bin/dovecot-auth-script.sh
     }
 
