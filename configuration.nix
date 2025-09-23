87163856cdf64242c0118446e475cfb551238634
Inex Code <inex.code@selfprivacy.org>
2025-09-23 18:12:01 +0300

    fix: Add password field to telemetry

--- configuration.nix
+++ configuration.nix
@@ -74,6 +74,7 @@ in
       endpoint = config.selfprivacy.telemetry.endpoint;
       basicAuth = {
         username = config.selfprivacy.telemetry.basicAuth.username;
+        password = config.selfprivacy.telemetry.basicAuth.password;
         passwordFile = config.selfprivacy.telemetry.basicAuth.passwordFile;
       };
       sampleRate = 1.0;
