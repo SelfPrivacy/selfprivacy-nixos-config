5d0e5f719e7d20c0023d51bbea8da959b7a4bd0b
Thary <thary@riseup.net>
2025-10-10 14:55:27 +0300

    feat(hedgedoc): don't recompile hedgedoc to apply patch (#178)
    Set the OAuth2 client secret via environment variables to eliminate patching of the HedgeDoc and avoid compilation.
    
    Reviewed-on: https://git.selfprivacy.org/SelfPrivacy/selfprivacy-nixos-config/pulls/178
    Reviewed-by: nhnn <nhnn@noreply.git.selfprivacy.org>
    Reviewed-by: Inex Code <inex.code@selfprivacy.org>
    Co-authored-by: Thary <thary@riseup.net>
    Co-committed-by: Thary <thary@riseup.net>

--- sp-modules/hedgedoc/module.nix
+++ sp-modules/hedgedoc/module.nix
@@ -13,6 +13,9 @@ let
   oauth2-provider-origin = config.services.kanidm.serverSettings.origin;
   usersGroup = "sp.hedgedoc.users";
   oauthClientSecretFP = auth-passthru.mkOAuth2ClientSecretFP oauthClientID;
+
+  secretsDir = "/run/keys/hedgedoc";
+  envFile = "${secretsDir}/oauth-clientsecret.env";
 in
 {
   options.selfprivacy.modules.hedgedoc = {
@@ -104,13 +107,7 @@ in
 
     services.hedgedoc = {
       enable = true;
-      package = pkgs.hedgedoc.overrideAttrs (old: {
-        doCheck = true;
-        patches = (old.patches or [ ]) ++ [
-          ./hedgedocClientSecretAsFile.patch
-        ];
-      });
-
+      environmentFile = envFile;
 
       settings = {
         port = 3001;
@@ -137,7 +134,6 @@ in
         oauth2 = {
           baseURL = oauth2-provider-origin;
           clientID = "hedgedoc";
-          clientSecret = oauthClientSecretFP;
           authorizationURL = "${oauth2-provider-origin}/ui/oauth2";
           tokenURL = "${oauth2-provider-origin}/oauth2/token";
           userProfileURL = "${oauth2-provider-origin}/oauth2/openid/${oauthClientID}/userinfo";
@@ -163,7 +159,23 @@ in
     };
 
     systemd = {
+      services.hedgedoc-secrets = let
+        hedgedoc-service-config = config.systemd.services.hedgedoc.serviceConfig;
+      in {
+        enable = true;
+        wantedBy = [ "multi-user.target" "hedgedoc.service" ];
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = pkgs.writeShellScript "gen-hedgedoc-oauth2clientsecret-envfile" ''
+            mkdir -p ${secretsDir} || true
+            echo "CMD_OAUTH2_CLIENT_SECRET=$(cat ${oauthClientSecretFP})" > ${envFile}
+            chmod 400 ${envFile}
+            chown ${hedgedoc-service-config.User}:${hedgedoc-service-config.Group} ${envFile}
+          '';
+        };
+      };
       services.hedgedoc = {
+        wants = [ "hedgedoc-secrets.service" ];
         unitConfig.RequiresMountsFor = lib.mkIf sp.useBinds "/volumes/${cfg.location}/hedgedoc";
         serviceConfig.Slice = "hedgedoc.slice";
       };
