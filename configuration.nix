893c71b4f1f1f567fa05437c8b73e750ee19f21d
nhnn <nhnn@nhnn.dev>
2025-12-10 12:12:10 +0300

    fix: try switching to Lix and not buggy version of Nix

--- configuration.nix
+++ configuration.nix
@@ -203,7 +203,7 @@ in
       fi
     '';
   nix = {
-    package = pkgs.nixVersions.nix_2_26;
+    package = pkgs.lixPackageSets.latest.lix;
     channel.enable = false;
 
     # daemonCPUSchedPolicy = "idle";
