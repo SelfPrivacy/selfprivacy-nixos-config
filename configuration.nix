dd7e969f147b229d9f28d06241cd99d1189ebeae
nhnn <nhnn@nhnn.dev>
2025-12-19 15:29:46 +0300

    fix: use default nix version for now

--- configuration.nix
+++ configuration.nix
@@ -202,7 +202,7 @@ in
     '';
   system.rebuild.enableNg = true;
   nix = {
-    package = pkgs.lixPackageSets.latest.lix;
+    #package = pkgs.lixPackageSets.latest.lix;
     channel.enable = false;
 
     # daemonCPUSchedPolicy = "idle";
