81d66916351387459d34226baa4d9d5c8bd0fed9
nhnn <nhnn@nhnn.dev>
2026-01-14 14:39:12 +0300

    chore: update nextcloud version to 32

--- sp-modules/nextcloud/module.nix
+++ sp-modules/nextcloud/module.nix
@@ -180,7 +180,7 @@ in
         };
         services.nextcloud = {
           enable = true;
-          package = pkgs.nextcloud31;
+          package = pkgs.nextcloud32;
           inherit hostName;
 
           # Use HTTPS for links
