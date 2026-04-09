83fb2a51afa0587d7d791d8d5026992a404d82d6
nhnn <nhnn@nhnn.dev>
2026-04-09 13:23:27 +0000

    chore: bump nixpkgs (#212)
    Reviewed-on: https://git.selfprivacy.org/SelfPrivacy/selfprivacy-nixos-config/pulls/212
    Reviewed-by: Inex Code <inex.code@selfprivacy.org>
    Co-authored-by: nhnn <nhnn@nhnn.dev>
    Co-committed-by: nhnn <nhnn@nhnn.dev>

--- sp-modules/vikunja/module.nix
+++ sp-modules/vikunja/module.nix
@@ -112,15 +112,13 @@ in
           local.enabled = false;
           openid = {
             enabled = true;
-            providers = [
-              {
-                name = oauth2-provider-name;
-                authurl = lib.strings.removeSuffix "/.well-known/openid-configuration" oauthDiscoveryURL;
-                clientid = oauthClientID;
-                clientsecret = ""; # There's patch for our Vikunja to make it load client secret from environment variable.
-                scope = "openid profile email";
-              }
-            ];
+            providers.kanidm = {
+              name = oauth2-provider-name;
+              authurl = lib.strings.removeSuffix "/.well-known/openid-configuration" oauthDiscoveryURL;
+              clientid = oauthClientID;
+              clientsecret = "empty"; # There's patch for our Vikunja to make it load client secret from environment variable.
+              scope = "openid profile email";
+            };
           };
         };
       };
