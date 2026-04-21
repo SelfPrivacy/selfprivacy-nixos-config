ec80a147b2dfcf27eaa324295c0a5f1aa9894e5a
nhnn <nhnn@nhnn.dev>
2026-04-21 13:16:06 +0300

    fix: refactor gts module

--- sp-modules/gotosocial/module.nix
+++ sp-modules/gotosocial/module.nix
@@ -2,6 +2,7 @@
   config,
   lib,
   pkgs,
+  selfprivacy,
   ...
 }:
 let
@@ -16,41 +17,9 @@ let
 in
 {
   options.selfprivacy.modules.gotosocial = {
-    enable =
-      (lib.mkOption {
-        default = false;
-        type = lib.types.bool;
-        description = "Enable GoToSocial";
-      })
-      // {
-        meta = {
-          type = "enable";
-        };
-      };
-    location =
-      (lib.mkOption {
-        type = lib.types.str;
-        description = "GoToSocial location";
-      })
-      // {
-        meta = {
-          type = "location";
-        };
-      };
-    subdomain =
-      (lib.mkOption {
-        default = "gts";
-        type = lib.types.strMatching "[A-Za-z0-9][A-Za-z0-9\-]{0,61}[A-Za-z0-9]";
-        description = "Subdomain";
-      })
-      // {
-        meta = {
-          widget = "subdomain";
-          type = "string";
-          regex = "[A-Za-z0-9][A-Za-z0-9\-]{0,61}[A-Za-z0-9]";
-          weight = 0;
-        };
-      };
+    enable = selfprivacy.types.enableOption "GoToSocial";
+    location = selfprivacy.types.locationOption;
+    subdomain = selfprivacy.types.subdomainOption "gts";
 
     appName =
       (lib.mkOption {
