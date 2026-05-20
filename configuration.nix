SelfPrivacy/selfprivacy-nixos-config

commit f4b5ef270d75c23f2fddcd3def5e8e14c323ee65

feat: selfprivacy helpers for modules (#211)
PoC for https://git.selfprivacy.org/SelfPrivacy/selfprivacy-nixos-config/pulls/208#issuecomment-12843

Reviewed-on: https://git.selfprivacy.org/SelfPrivacy/selfprivacy-nixos-config/pulls/211
Reviewed-by: Inex Code <inex.code@selfprivacy.org>

flake.nix
@@ -49,6 +49,7 @@
             modules = [
               hardware-configuration
               deployment
+              ./modules
               ./configuration.nix
               selfprivacy-api.nixosModules.default
               (
@@ -118,12 +119,17 @@
                 args@{ config, pkgs, ... }:
                 let
                   lib = nixpkgs.lib;
+                  # workaround for infinite recursion because mailserver defines selfprivacy.xxx options but also depends on selfprivacy argument for OIDC helpers.
+                  selfprivacyModuleArg = {
+                    inherit ((import ./modules/types.nix { inherit lib; }).selfprivacy.passthru) types;
+                  };
                   configPathsNeeded =
                     sp-module.configPathsNeeded or (abort "allowed config paths not set for module \"${name}\"");
                   constrainConfigArgs =
                     args'@{ pkgs, ... }:
                     args'
                     // {
+                      selfprivacy = selfprivacyModuleArg;
                       config =
                         # TODO use lib.attrsets.mergeAttrsList from nixpkgs 23.05
                         (

lib/meta.nix
@@ -1,8 +1,18 @@
 { sp-module, pkgs }:
 let
   lib = pkgs.lib;
+  selfprivacy =
+    (pkgs.lib.evalModules {
+      modules = [
+        ../selfprivacy-module.nix
+        ../modules
+      ];
+    }).config.selfprivacy.passthru;
   options =
     (pkgs.lib.evalModules {
+      specialArgs = {
+        inherit selfprivacy;
+      };
       modules = [
         { _module.check = false; }
         sp-module.nixosModules.default
@@ -19,10 +29,10 @@ let
     }
   );
 in
-builtins.toJSON ({
+builtins.toJSON {
   meta = if builtins.hasAttr "meta" sp-module then sp-module.meta { inherit lib; } else null;
   configPathsNeeded = sp-module.configPathsNeeded;
   options = pkgs.lib.mapAttrs optionToMeta (
     builtins.head (lib.mapAttrsToList (name: value: value) options.selfprivacy.modules)
   );
-})
+}

checks/system-eval.nix
@@ -13,38 +13,42 @@ let
     builtins.readDir ../sp-modules
   );
 in
-(self.nixosConfigurations-fun {
-  hardware-configuration = {
-    system.stateVersion = lib.trivial.release;
-    nixpkgs.hostPlatform = system;
-    boot.loader.grub.device = "/dev/sda";
-    fileSystems."/" = {
-      device = "/dev/sda1";
-      fsType = "ext4";
-    };
-  };
-  userdata = {
-    dns = {
-      provider = "CLOUDFLARE";
-      useStagingACME = false;
-    };
-    server.provider = "HETZNER";
-    domain = "dummy.site";
-    hashedMasterPassword = "$6$aaaaaaaaaaaaaaa$aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-    hostname = "dummysite";
-    timezone = "Etc/UTC";
-    username = "user";
-    useBinds = true;
-    sshKeys = [ ];
-    users = [ ];
-    autoUpgrade.enable = true;
-    postgresql.location = "sda";
-    modules = lib.mapAttrs (_: _: {
-      enable = true;
-      location = "sda";
-    }) sp-modules;
-  };
-  deployment = { };
-  top-level-flake = self;
-  inherit sp-modules;
-}).default.config.system.build.toplevel
+let
+  config =
+    (self.nixosConfigurations-fun {
+      hardware-configuration = {
+        system.stateVersion = lib.trivial.release;
+        nixpkgs.hostPlatform = system;
+        boot.loader.grub.device = "/dev/sda";
+        fileSystems."/" = {
+          device = "/dev/sda1";
+          fsType = "ext4";
+        };
+      };
+      userdata = {
+        dns = {
+          provider = "CLOUDFLARE";
+          useStagingACME = false;
+        };
+        server.provider = "HETZNER";
+        domain = "dummy.site";
+        hashedMasterPassword = "$6$aaaaaaaaaaaaaaa$aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        hostname = "dummysite";
+        timezone = "Etc/UTC";
+        username = "user";
+        useBinds = true;
+        sshKeys = [ ];
+        users = [ ];
+        autoUpgrade.enable = true;
+        postgresql.location = "sda";
+        modules = lib.mapAttrs (_: _: {
+          enable = true;
+          location = "sda";
+        }) sp-modules;
+      };
+      deployment = { };
+      top-level-flake = self;
+      inherit sp-modules;
+    }).default;
+in
+config.config.system.build.toplevel // { config = config; }

modules/default.nix
@@ -1,0 +1,5 @@
+{
+  imports = [
+    ./types.nix
+  ];
+}

modules/types.nix
@@ -1,0 +1,57 @@
+{ lib, ... }:
+let
+  overridableOptionDefinition =
+    optionDef: metaDef:
+    (lib.mkOption optionDef)
+    // {
+      meta = metaDef;
+      __functor =
+        _:
+        {
+          option ? { },
+          meta ? { },
+        }:
+        overridableOptionDefinition (optionDef // option) (metaDef // meta);
+    };
+in
+{
+  selfprivacy.passthru.types = rec {
+    subdomainType = lib.types.strMatching "[A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9]";
+    subdomainOption =
+      default:
+      overridableOptionDefinition
+        {
+          inherit default;
+          type = subdomainType;
+          description = "Subdomain";
+        }
+        {
+          widget = "subdomain";
+          type = "string";
+          regex = "[A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9]";
+          weight = 0;
+        };
+
+    enableOption =
+      serviceName:
+      overridableOptionDefinition
+        {
+          default = false;
+          type = lib.types.bool;
+          description = "Enable ${serviceName}";
+        }
+        {
+          type = "enable";
+        };
+
+    locationOption =
+      overridableOptionDefinition
+        {
+          type = lib.types.str;
+          description = "Data location";
+        }
+        {
+          type = "location";
+        };
+  };
+}

selfprivacy-module.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, config, ... }:
 
 with lib;
 {
@@ -215,5 +215,9 @@ with lib;
         default = false;
       };
     };
+  };
+
+  config = {
+    _module.args.selfprivacy = config.selfprivacy.passthru;
   };
 }

sp-modules/actual/module.nix
@@ -2,6 +2,7 @@
   config,
   lib,
   pkgs,
+  selfprivacy,
   ...
 }:
 let
@@ -33,41 +34,9 @@ let
 in
 {
   options.selfprivacy.modules.actual = {
-    enable =
-      (lib.mkOption {
-        default = false;
-        type = lib.types.bool;
-        description = "Enable the Actual Budget server";
-      })
-      // {
-        meta = {
-          type = "enable";
-        };
-      };
-    location =
-      (lib.mkOption {
-        type = lib.types.str;
-        description = "Data location";
-      })
-      // {
-        meta = {
-          type = "location";
-        };
-      };
-    subdomain =
-      (lib.mkOption {
-        default = "actual";
-        type = lib.types.strMatching "[A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9]";
-        description = "Subdomain";
-      })
-      // {
-        meta = {
-          widget = "subdomain";
-          type = "string";
-          regex = "[A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9]";
-          weight = 0;
-        };
-      };
+    enable = selfprivacy.types.enableOption "Actual Budget";
+    location = selfprivacy.types.locationOption;
+    subdomain = selfprivacy.types.subdomainOption "actual";
     # service settings
     enableDebug =
       (lib.mkOption {

sp-modules/bitwarden/module.nix
@@ -2,6 +2,7 @@
   config,
   lib,
   pkgs,
+  selfprivacy,
   ...
 }:
 let
@@ -12,41 +13,9 @@ let
 in
 {
   options.selfprivacy.modules.bitwarden = {
-    enable =
-      (lib.mkOption {
-        default = false;
-        type = lib.types.bool;
-        description = "Enable Vaultwarden";
-      })
-      // {
-        meta = {
-          type = "enable";
-        };
-      };
-    location =
-      (lib.mkOption {
-        type = lib.types.str;
-        description = "Vaultwarden location";
-      })
-      // {
-        meta = {
-          type = "location";
-        };
-      };
-    subdomain =
-      (lib.mkOption {
-        default = "password";
-        type = lib.types.strMatching "[A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9]";
-        description = "Subdomain";
-      })
-      // {
-        meta = {
-          widget = "subdomain";
-          type = "string";
-          regex = "[A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9]";
-          weight = 0;
-        };
-      };
+    enable = selfprivacy.types.enableOption "Vaultwarden";
+    location = selfprivacy.types.locationOption;
+    subdomain = selfprivacy.types.subdomainOption "password";
     signupsAllowed =
       (lib.mkOption {
         default = true;

commit e19276a293aaae5fea0a3b6bee15769d2d9dc6f8

fix: pass selfprivacy config path to lib/meta.nix (#225)
fixes regression that broke remote modules in #211

Reviewed-on: https://git.selfprivacy.org/SelfPrivacy/selfprivacy-nixos-config/pulls/225
Reviewed-by: Inex Code <inex.code@selfprivacy.org>

configuration.nix
@@ -56,7 +56,10 @@ in
       { flakeURL }: let
         sp-module = builtins.getFlake flakeURL;
         pkgs = import ${pkgs.path} {};
-      in (import ${./lib/meta.nix}) { inherit pkgs sp-module; }
+      in (import ${./lib/meta.nix}) {
+        selfprivacyConfig = ${./.};
+        inherit pkgs sp-module;
+      }
     '';
   };
 

flake.nix
@@ -58,7 +58,10 @@
                   environment.etc =
                     (lib.attrsets.mapAttrs' (name: sp-module: {
                       name = "sp-modules/${name}";
-                      value.text = import ./lib/meta.nix { inherit pkgs sp-module; };
+                      value.text = import ./lib/meta.nix {
+                        selfprivacyConfig = ./.;
+                        inherit pkgs sp-module;
+                      };
                     }) sp-modules)
                     // {
                       suggested-sp-modules.text = builtins.toJSON (builtins.attrNames (builtins.readDir ./sp-modules));

lib/meta.nix
@@ -1,11 +1,15 @@
-{ sp-module, pkgs }:
+{
+  selfprivacyConfig,
+  sp-module,
+  pkgs,
+}:
 let
   lib = pkgs.lib;
   selfprivacy =
     (pkgs.lib.evalModules {
       modules = [
-        ../selfprivacy-module.nix
-        ../modules
+        (selfprivacyConfig + "/selfprivacy-module.nix")
+        (selfprivacyConfig + "/modules")
       ];
     }).config.selfprivacy.passthru;
   options =